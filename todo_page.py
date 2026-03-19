25dc20f8534f60be7e1ebd9d13f820361a546346
dhruv <88807613+dhruvavibhu1@users.noreply.github.com>
2026-03-19 23:53:56 +0000

    finished writing execute queries

--- todo_page.py
+++ todo_page.py
@@ -1,16 +1,20 @@
-import sqlite3
-# pyright: ignore[reportMissingImports]
+"""Creates a web page to display the todo list"""
+# imports the sqlite3 library, which allows us to interact with SQLite databases
+import sqlite3]
 from bottle import route, run
+
+def execute_query(query, params=(), fetch=False):
+    with sqlite3.connect('todo.db') as conn:
+        cur = conn.cursor()
+        cur.execute(query, params)
+        return cur.fetchall() if fetch else None
 
 @route('/')
 def todo_list():
-    conn = sqlite3.connect('todo.db')
-    cur = conn.cursor()
+    rows = execute_query("SELECT category, item FROM todo", fetch=True)
 
-    cur.execute("SELECT * FROM todo")
-    result = cur.fetchall()
-    cur.close()
-
-    return str(result)
+    table_rows = ""
+    for row in rows:
+        row_id, catagory, item = row
 
 run(host='localhost', port=8080)
